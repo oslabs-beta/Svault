583920b9299f8aa70e3107c06c797bee3f67351c
fbiswas <fpena213@gmail.com>
2023-06-29 22:00:45 -0500

    adds routes/signup svelte page

---
+++ src/routes/signup/+page.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import type { ActionData } from './$types';
+  </script>
+  
+  <div class="container">
+    <h1 class="is-size-3 has-text-weight-semibold my-4">Login or Register</h1>
+    <form method="post">
+      <input class="input my-2" type="text" placeholder="Username" name="username" required />
+      <input class="input my-2" type="password" placeholder="Password" name="password" required />
+  
+      <button class="button mt-4 mr-3" type="submit" formaction="?/register">Register</button>
+      <button class="button is-primary mt-4" type="submit" formaction="?/login">Login</button>
+    </form>
+  </div>
