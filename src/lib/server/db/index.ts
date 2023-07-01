392de52a3ea7699b3a8365f78948d1b7d8279efb
missmshel <mshelconroy@gmail.com>
2023-07-01 17:16:27 -0600

    creates branch, reconciles first native-auth and oauth merge, runs build and installs dependencies

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -6,24 +6,24 @@ const db = require('./models');
 //  * const db = new Database <import name>
   */
 
-// Function to create user. 
+// Function to create user.
 export async function createUser(
-    username: string,
-    password: string
+  username: string,
+  password: string
 ): Promise<void> {
-    // 'sql' can be renamed, values can also be modified to meet your database needs and ensure query parity.
-    //tutorial inserted the unhashed password into the db, but i adjusted the db insertion to only include the hashpassword for safety reasons.. what do you think?
-    const sql = `
+  // 'sql' can be renamed, values can also be modified to meet your database needs and ensure query parity.
+  //tutorial inserted the unhashed password into the db, but i adjusted the db insertion to only include the hashpassword for safety reasons.. what do you think?
+  const sql = `
     insert into users (username, password)
     values ($username, $hashpassword)
     `;
-    //adjust workFactor value to your needs, should we use a type alias here?
-    const workFactor = 10;
-    const hashPassword = await bcrypt.hash(password, workFactor);
-    
-    const stmnt = db.query(sql);
-   //stmnt.run({ username, hashpassword: hashPassword });
-    console.log('Successfully created user!')
+  //adjust workFactor value to your needs, should we use a type alias here?
+  const workFactor = 10;
+  const hashPassword = await bcrypt.hash(password, workFactor);
+
+  const stmnt = db.query(sql);
+  //stmnt.run({ username, hashpassword: hashPassword });
+  console.log('Successfully created user!');
 }
 
 //function to check user credentials
@@ -32,43 +32,35 @@ export async function checkUserCredentials(
   username: string,
   password: string
 ): Promise<boolean> {
-    const sql = `
+  const sql = `
     select password
       from users
      where username = $username
     `;
-    const stmnt = db.prepare(sql);
-    //can we use object destructing here intead?
-    //ex. const { username } = stmnt;
-    const row = stmnt.get({ username });
-    if (row) {
-      return bcrypt.compare(password, row.password);
-    } else {
-      // spend some time to "waste" some time
-      // this makes brute forcing harder
-      // could also do a timeout here
-      //adjust workFactor value to your needs
-      const workFactor = 10;
-      await bcrypt.hash(password, workFactor);
-      return false;
-    }
+  const stmnt = db.prepare(sql);
+  //can we use object destructing here intead?
+  //ex. const { username } = stmnt;
+  const row = stmnt.get({ username });
+  if (row) {
+    return bcrypt.compare(password, row.password);
+  } else {
+    // spend some time to "waste" some time
+    // this makes brute forcing harder
+    // could also do a timeout here
+    //adjust workFactor value to your needs
+    const workFactor = 10;
+    await bcrypt.hash(password, workFactor);
+    return false;
+  }
 }
-
-
-
-
-
-
-
-
 
 // const username = name;
 // bcrypt
 // .hash(password, workFactor)
 // .then(hash => {
 //     console.log('line 13', hash)
-//     const query = `UPDATE users 
-//     SET hashedpassword = '${hash}' 
+//     const query = `UPDATE users
+//     SET hashedpassword = '${hash}'
 //     WHERE username = '${username}';`
 //     db.query(query)
 //       return next();
--- src/lib/server/db/models.ts
+++ src/lib/server/db/models.ts
@@ -1,10 +1,15 @@
 import { Pool } from 'pg';
+import { PG_URI } from '$env/static/private';
 
-const PG_URI = 'postgres://olsxjxwa:z-vPhW0jFxZlp-R0NuyQDf2VDiIyZ1PT@stampy.db.elephantsql.com/olsxjxwa';
 // create a new pool here using the connection string above
-const pool = new Pool({
-  connectionString: PG_URI
-})
+const db = new Pool({
+  connectionString: PG_URI,
+});
+
+db.query = (text, params, callback) => {
+  console.log('executed query', text);
+  return db.query(text, params, callback);
+};
 
 // Schema for the database can be found below:
 // https://github.com/CodesmithLLC/unit-10SB-databases/blob/master/docs/assets/images/schema.png
@@ -12,9 +17,4 @@ const pool = new Pool({
 // We export an object that contains a property called query,
 // which is a function that returns the invocation of pool.query() after logging the query
 // This will be required in the controllers to be the access point to the database
-module.exports = {
-  query: (text: string, params, callback) => {
-    console.log('executed query', text)
-    return pool.query(text, params, callback)
-  }
-}
+export default db;
