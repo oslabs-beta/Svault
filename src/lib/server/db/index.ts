15f28d731435f1746c62bee75e9bc0e0c3035a97
fbiswas <fpena213@gmail.com>
2023-07-01 18:58:10 -0500

    moves db credentials to env, changes to models, changes to index.ts, changes to page.server.ts

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,7 @@
 /** need to create import to your database */
 import bcrypt from 'bcrypt';
-import db from 'models';
+import db from './models.js';
 
-/** db set up
-//  * const db = new Database <import name>
-  */
 
 // Function to create user. 
 export async function createUser(
@@ -12,7 +9,6 @@ export async function createUser(
     password: string
 ): Promise<void> {
     // 'sql' can be renamed, values can also be modified to meet your database needs and ensure query parity.
-    //tutorial inserted the unhashed password into the db, but i adjusted the db insertion to only include the hashpassword for safety reasons.. what do you think?
     const sql = `
     insert into users (username, password)
     values ($username, $hashpassword)
@@ -21,8 +17,8 @@ export async function createUser(
     const workFactor = 10;
     const hashPassword = await bcrypt.hash(password, workFactor);
     
-    const stmnt = db.query(sql);
-   //stmnt.run({ username, hashpassword: hashPassword });
+    db.query(sql);
+  //  stmnt.run({ username, hashpassword: hashPassword });
     console.log('Successfully created user!')
 }
 
@@ -31,51 +27,27 @@ export async function createUser(
 export async function checkUserCredentials(
   username: string,
   password: string
-): Promise<boolean> {
-    const sql = `
+): Promise<any> {
+    const queryString = `
     select password
       from users
      where username = $username
     `;
-    const stmnt = db.prepare(sql);
-    //can we use object destructing here intead?
-    //ex. const { username } = stmnt;
-    const row = stmnt.get({ username });
-    if (row) {
-      return bcrypt.compare(password, row.password);
-    } else {
-      // spend some time to "waste" some time
-      // this makes brute forcing harder
-      // could also do a timeout here
-      //adjust workFactor value to your needs
-      const workFactor = 10;
-      await bcrypt.hash(password, workFactor);
-      return false;
-    }
+    db.query(queryString)
+    .then((data) => { 
+      //Send username to frontend
+      console.log('Data.rows from db.query on log in', data);
+      if (data.rows[0]) {
+        return bcrypt.compare(password, data.rows[0].password);
+      } else {
+        //this means the username doesn't exist in the db but dont tell the client that
+        // spend some time to "waste" some time
+        // this makes brute forcing harder
+        // could also do a timeout here
+        //adjust workFactor value to your needs
+        const workFactor = 10;
+        bcrypt.hash(password, workFactor);
+        return false;
+      }
+    });
 }
-
-
-
-
-
-
-
-
-
-// const username = name;
-// bcrypt
-// .hash(password, workFactor)
-// .then(hash => {
-//     console.log('line 13', hash)
-//     const query = `UPDATE users 
-//     SET hashedpassword = '${hash}' 
-//     WHERE username = '${username}';`
-//     db.query(query)
-//       return next();
-//     })
-//     .catch((err) => {
-//       return next({
-//         log: `Error in userController.create:', ${err}`,
-//         message: { err: 'Error occured in userController.create' }
-//       })
-//     })
--- src/lib/server/db/models.ts
+++ src/lib/server/db/models.ts
@@ -2,19 +2,18 @@ import { Pool } from 'pg';
 import { PG_URI } from "$env/static/private";
 
 // create a new pool here using the connection string above
-const pool = new Pool({
+const db = new Pool({
   connectionString: PG_URI
-})
+});
 
-// Schema for the database can be found below:
+db.query = (text: string, params?, callback?) => {
+  console.log('executed query', text)
+  return db.query(text, params, callback)
+};
+// Schema for the  database can be found below:
 // https://github.com/CodesmithLLC/unit-10SB-databases/blob/master/docs/assets/images/schema.png
 
 // We export an object that contains a property called query,
 // which is a function that returns the invocation of pool.query() after logging the query
 // This will be required in the controllers to be the access point to the database
-module.exports = {
-  query: (text: string, params, callback) => {
-    console.log('executed query', text)
-    return pool.query(text, params, callback)
-  }
-}
+export default db;
