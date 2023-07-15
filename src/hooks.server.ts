9107e2c4c569de53d628ecbfd1985e0ca6f16824
trisbt <tristanbott30@gmail.com>
2023-07-15 15:43:27 -0700

    oauth update before merge

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,9 +1,9 @@
 import { github } from './lib/server/oauth/github/api/github.ts';
 import { google } from './lib/server/oauth/google/api/google.ts';
-import { facebook } from './lib/server/oauth/facebook/api/facebook.ts';
+// import { facebook } from './lib/server/oauth/facebook/api/facebook.ts';
 import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
 import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
-import { FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET } from '$env/static/private';
+// import { FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET } from '$env/static/private';
 import { sequence } from '@sveltejs/kit/hooks';
 
 
@@ -13,9 +13,9 @@ const redirectPath = '/secret'
 const googleCallback = 'http://localhost:5173/oauth/google/validate'
 export const first = google(GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, redirectPath, googleCallback)
 export const second = github(GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, redirectPath)
-export const third = facebook(FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, redirectPath)
+// export const third = facebook(FACEBOOK_CLIENT_ID, FACEBOOK_CLIENT_SECRET, redirectPath)
 
-export const handle = sequence(first, second, third);
+export const handle = sequence(first, second);
 
 
 
